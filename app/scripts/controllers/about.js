bce08eb4e280f8b0e28b782281d72d39f0e70333
Tony <yong1236@foxmail.com>
2015-11-21 02:14:29 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ilearnSnsFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ilearnSnsFrontApp
+ */
+angular.module('ilearnSnsFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ilearnSnsFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ilearnSnsFrontApp
+ */
+angular.module('ilearnSnsFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
